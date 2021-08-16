1cabfca7c9535e28d70765785a1ff5bdbefbeffe
Rick Brown <ricky.brown.00@gmail.com>
2021-08-16 21:19:16 +0100

    adds downloading coin details

--- crypto-tracker/Core/Detail/Views/DetailView.swift
+++ crypto-tracker/Core/Detail/Views/DetailView.swift
@@ -20,10 +20,14 @@ struct DetailLoadingView: View {
 }
 
 struct DetailView: View {
-  let coin: CoinModel
+  @StateObject var vm : DetailViewModel
+  
+  init(coin: CoinModel) {
+    _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+  }
   
   var body: some View {
-        Text(coin.name)
+        Text("OK ")
   }
 }
 
